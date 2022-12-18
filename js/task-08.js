1d8ec3183d6a7600f06949a0329e756b597a60de
polrmn <polrmn@gmail.com>
2022-12-18 23:19:12 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
+const formRef = document.querySelector('.login-form');
 
+// console.log(formRef.elements);
+
+formRef.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+
+    event.preventDefault();
+
+
+
+    if (event.currentTarget.elements.email.value !== '' && event.currentTarget.password.value !== '') {
+        
+        const formData = {
+            email: event.currentTarget.elements.email.value,
+            password: event.currentTarget.password.value,
+        }
+    
+        console.log(formData);
+        
+    } else {
+
+        alert("Please fill in all the fields!");
+        
+    }
+
+    event.currentTarget.reset();
+}
