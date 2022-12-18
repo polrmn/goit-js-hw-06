b8e0196c4e25b23ee733827a03455331ac405a91
polrmn <polrmn@gmail.com>
2022-12-18 23:41:48 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,21 @@
+const btnRef = document.querySelector('.change-color');
+const spanRef = document.querySelector('.color');
+
+btnRef.addEventListener('click', onBntClick);
+
+document.body.style.backgroundColor = getRandomHexColor();
+spanRef.textContent = document.body.style.backgroundColor;
+
+function onBntClick() {
+
+  document.body.style.backgroundColor = getRandomHexColor();
+
+  spanRef.textContent = document.body.style.backgroundColor;
+
+}
+
 function getRandomHexColor() {
+
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
 }
