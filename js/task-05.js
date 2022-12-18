4627d96aae57b8de7df5a0425b429b0cc5c7ab9a
polrmn <polrmn@gmail.com>
2022-12-18 21:49:14 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -3,4 +3,10 @@ const spanNameRef = document.querySelector('#name-output');
 
 inputRef.addEventListener('input', (event) => {
     spanNameRef.textContent = event.currentTarget.value;
+    
+    if (event.currentTarget.value === '') {
+
+        spanNameRef.textContent = 'Anonymous';
+        
+    }
 });
